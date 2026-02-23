e5a44945430d4204af51f68f4f006ffe957f37b0
N.S.Devaraj <nsdevaraj@gmail.com>
2026-02-23 15:00:12 +0530

    only ios code

--- ios/ViewController.h
+++
@@ -1,8 +0,0 @@
-#import <UIKit/UIKit.h>
-#import <WebKit/WebKit.h>
-
-@interface ViewController : UIViewController <WKNavigationDelegate, WKUIDelegate>
-
-@property (strong, nonatomic) WKWebView *webView;
-
-@end
